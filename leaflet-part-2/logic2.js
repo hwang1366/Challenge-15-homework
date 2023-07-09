a68b1cfba6457cf5bcdf7e8cbee85eacc8567dc7
hwang1366 <huiwanguh@gmail.com>
2023-07-09 13:37:06 -0500

    update2

--- leaflet-part-2/logic2.js
+++ leaflet-part-2/logic2.js
@@ -10,3 +10,21 @@ d3.json(queryUrl).then(function(earthquakeData) {
     });
 });
 
+function createMarker(feature, latlng) {
+  return L.circleMarker(latlng, {
+      radius: markerSize(feature.properties.mag),
+      fillColor: markerColor(feature.geometry.coordinates[2]),
+      color:"#000",
+      weight: 0.5,
+      opacity: 0.5,
+      fillOpacity: 1
+  });
+}
+function createFeatures(earthquakeData, plateData) {
+  function onEachFeature(feature, layer) {
+      layer.bindPopup(`<h3>Location:</h3> ${feature.properties.place}<h3> Magnitude:</h3> ${feature.properties.mag}<h3> Depth:</h3> ${feature.geometry.coordinates[2]}`);
+  }}
+  let earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+    pointToLayer: createMarker
+});
