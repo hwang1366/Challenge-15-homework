067a2178aadda005604eb848515a56403ca8c0c5
hwang1366 <huiwanguh@gmail.com>
2023-07-09 13:39:15 -0500

    update4

--- leaflet-part-2/logic2.js
+++ leaflet-part-2/logic2.js
@@ -38,3 +38,31 @@ let plates = L.geoJSON(plateData, {
 });
 
 createMap(earthquakes, plates);
+function createMap(earthquakes, plates) {
+  
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+
+  
+  let baseMaps = {
+      "Street Map": street,
+      "Topographic Map": topo
+  };
+
+  
+  let overlayMaps = {
+      "Earthquakes": earthquakes,
+      "Fault Lines": plates
+  };
+
+  
+  let myMap = L.map("map", {
+      center: [37.09, -95.71],
+      zoom: 5,
+      layers: [street, earthquakes, plates]
+  });
